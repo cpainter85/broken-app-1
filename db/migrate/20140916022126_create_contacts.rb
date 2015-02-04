f7b45023bdda62c84128e57c828dd8c8bc585ca5
Charles Painter <cpainter85@gmail.com>
2015-02-04 10:23:55 -0700

    working app

--- db/migrate/20140916022126_create_contacts.rb
+++ db/migrate/20140916022126_create_contacts.rb
@@ -1,12 +1,12 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      ContctUs.load_data
-      t.integer :name
+      # Contact.load_data
+      t.string :name
       t.string :email
       t.text :notes
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
